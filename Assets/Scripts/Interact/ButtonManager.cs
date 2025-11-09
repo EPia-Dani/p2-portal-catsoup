289cb10df3819b6b435ccfe9b07bd2b1819323af
Nai <ngarciad@edu.tecnocampus.cat>
2025-11-09 23:49:49 +0100

    button and Spawner work

--- Assets/Scripts/Interact/ButtonManager.cs
+++ Assets/Scripts/Interact/ButtonManager.cs
@@ -4,27 +4,29 @@ using UnityEngine;
 
 public class ButtonManager : MonoBehaviour
 {
-    public IActionManager actionObject;
     public Animator animator;
-    private bool isPressed, wasPressed;
+    public bool isPressed, wasPressed;
+    public SpawnerManager spawner;
     
     private void OnTriggerEnter(Collider interactable)
     {
         if (isPressed) return;
         if(!interactable.CompareTag("Interactable")) return;
-        
         isPressed = true;
+        
         if(animator != null)
         {
             animator.SetTrigger("ButtonPressed");
             Debug.Log("Button pressed, playing animation.");
+            spawner.performAction();
         }
         else
         { 
             Debug.LogWarning("Animator not assigned on ButtonManager.");
         }
-        actionObject?.performAction();
         wasPressed = true;
+        
+        
     }
     private void OnTriggerExit(Collider interactable)
     {
